345f5891a48faf2c4bc560e34dd0749d8f47c31f
Ahlam Mamoun <mestar.ale@gmail.com>
2022-11-07 15:13:06 +0100

    form with method post

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# Form-php
-## Form with method 'GET' 
+# Form-php-Get
+
+## Form with method 'GET'
+
 ## Validate Value of the input
+
 ## Transtype the value
-## Treatment rules for the rate of the assurance 
+
+## Treatment rules for the rate of the assurance
+
+### Form-php-Post
+
+## Form with method 'POST'
+
+## Validation of the data
+
+## Condition of the rate of the rent
---
+++ form-post/html/index.php
@@ -0,0 +1,108 @@
+<?php
+
+//I include the file functions
+require __DIR__ . '/../inc/functions.php';
+
+
+// 1. Créer un formulaire HTML demandant les informations nécessaires au calcul
+// 2. Après soumission du formulaire, créer les variables qui vont contenir les informations du client
+// 3. Écrire le script qui permet de déterminer à quel palier peut prétendre un client, selon ses informations (et donc à quelle couleur de tarif)
+// 4. Afficher le résultat, par ex. "Votre client a droit au tarif Vert"
+// Bonus 1. Afficher le résultat de trois manières différentes : via `if` & `elseif` ou bien `switch` ou bien `array()`
+// Bonus 2. fioritures graphiques
+
+
+//I check if the form was sent
+if (!empty($_POST)) {
+
+    //var_dump($_POST);
+
+
+
+    // I get the informations and put them in a variable , and a value by default
+    $age = (int) $_POST['age'] ?? false;
+    $mention = (int) $_POST['mention'] ?? false;
+    $salaire = (int) $_POST['salaire'] ?? false;
+
+
+
+    //I check the data , are they valid ?
+    if (
+        $age !== false &&
+        $mention !== false &&
+        $salaire !== false
+    ) {
+
+    //I initiate a variable and put in it , the function and the values of the three variables
+        $numeroPalier = calculPalier($age, $mention, $salaire);
+
+
+    //I initiate an array with five different level 
+        $listePaliers = [
+            0 => 'Nous ne pouvons pas vous faire une offre',
+            1 => '150',
+            2 => '250',
+            3 => '350',
+            4 => '450'
+        ];
+
+    
+        $tarif = $listePaliers[$numeroPalier];
+        //var_dump($tarif);
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Logement universitaire</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div>
+            <h1>Logement universitaire</h1>
+            <h2>Calcul du loyer de l'étudiant</h2>
+
+
+            <!--if this tow variables exist I display their values-->
+            <?php if (isset($tarif) && isset($numeroPalier)) : ?>
+                <p>
+                    Votre client à droit au tarif suivant <strong class="palier<?=  $numeroPalier ?>"><?= $tarif . ' euros' ?></strong>
+                </p>
+            <?php endif; ?>
+
+            <form action="" method="post">
+
+
+                <label for="age">Âge</label>
+                <input id="age" type="number" name="age" value="" placeholder="" min="15" max="150" required>
+
+
+                <label for="mention">Dernière mention</label>
+                <input id="mention" type="number" name="mention" value="" placeholder="" min="0" max="150" required>
+
+
+                <label for="salaire">Salaire des parents</label>
+                <input type="number" name="salaire" value="" placeholder="" min="0" required>
+
+
+
+                <button>Calculer le loyer</button>
+            </form>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
