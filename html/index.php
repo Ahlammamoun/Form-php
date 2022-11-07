96d8a63d9ad8c5e228a36132dc6d3d6c533b697a
Ahlam Mamoun <mestar.ale@gmail.com>
2022-11-07 11:52:13 +0100

    calcul of the rate

--- html/index.php
+++ html/index.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
+
 </html>
 
 <body>
@@ -31,20 +32,20 @@
 
 
 
-        <!--I'm opening a php script , to check if the key 'lastename' exist in the 'GET' array and different to null, then I check if the user put something in the input -->
+            <!--I'm opening a php script , to check if the key 'lastename' exist in the 'GET' array and different to null, then I check if the user put something in the input -->
             <?php
-            if (isset($_GET['lastname']) && $_GET['lastname'] !=='') {
-        /*I initiate a variable 'lastname' I put in it the value of the key 'lastname' of the array 'GET'*/ 
-                $lastname =  $_GET['lastname']  ;
-        /*I display the varuiable concatener with en space */
+            if (isset($_GET['lastname']) && $_GET['lastname'] !== '') {
+                /*I initiate a variable 'lastname' I put in it the value of the key 'lastname' of the array 'GET'*/
+                $lastname =  $_GET['lastname'];
+                /*I display the varuiable concatener with en space */
                 echo $lastname  . '  ';
-            }else{
-        /*else I display a hyphen */
+            } else {
+                /*else I display a hyphen */
                 echo '-';
             };
-        /*I do the same for the firstname */
-            if (isset($_GET['firstname']) && $_GET['firstname'] !=='') {
-                $firstname =  $_GET['firstname']  ;
+            /*I do the same for the firstname */
+            if (isset($_GET['firstname']) && $_GET['firstname'] !== '') {
+                $firstname =  $_GET['firstname'];
                 echo $firstname;
             };
             ?>
@@ -52,6 +53,33 @@
         </div>
         <h2>Tarif </h2>
         <div class="answer">
+            <?php
+            /*all the informations are in 'string' type , we need to transtype to have an 'int' type */
+            /* I initiate a variable age , with filter_input (we get the value, validate and transtype) return null if age is not in array GET and false if we can't trastype*/
+
+            $age = filter_input(INPUT_GET, 'age', FILTER_VALIDATE_INT);
+
+            /*I made an algorithm to applicate rules to follow according to age*/
+            if ($age !== null && $age !== false) {
+                // if (isset($age) && $age !== false)  {
+                if ($age < 16) {
+                    $tarif = 'Nous ne pouvons pas vous assurer';
+                } elseif ($age >= 18 && $age <= 32) {
+                    $tarif = '35';
+                } elseif ($age > 32 && $age <= 42) {
+                    $tarif = '75';
+                } elseif ($age < 42 && $age <= 60) {
+                    $tarif = '108';
+                } else {
+                    $tarif = '135';
+                }
+            } else {
+                $tarif = '-';
+            }
+
+            echo $tarif;
+
+            ?>
 
         </div>
     </aside>
