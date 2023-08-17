bea095a0323d1b6dddef8039eb62d74a8eda7cf8
Ahlammamoun <mestar.ale@gmail.com>
2023-08-17 17:44:00 +0200

    path of the index changed

--- form-get/index.php
+++ form-get/index.php
@@ -6,7 +6,7 @@
     <title>Mutuelle</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans@1&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 </html>
